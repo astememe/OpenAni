3be74a66fbf76249a7a301f539ea42dfdf9fc8ad
rayden <raydenldon@gmail.com>
2026-02-13 12:05:31 +0100

    manifest ordenado

--- app/src/main/java/com/astememe/openani/LoginView.java
+++ app/src/main/java/com/astememe/openani/LoginView.java
@@ -1,14 +1,19 @@
 package com.astememe.openani;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginView extends AppCompatActivity {
+
+    ConstraintLayout boton_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,14 @@ public class LoginView extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        boton_login = findViewById(R.id.boton_login);
+        boton_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginView.this, MainAnime.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/astememe/openani/RegisterView.java
+++ app/src/main/java/com/astememe/openani/RegisterView.java
@@ -20,6 +20,5 @@ public class RegisterView extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
