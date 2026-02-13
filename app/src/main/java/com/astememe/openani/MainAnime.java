83692fd388bfe4d36aa716df28145d71c6f5ee7e
rayden <raydenldon@gmail.com>
2026-02-13 13:36:27 +0100

    añadiendo textview de magnet que tendrÃrá el boton download

--- app/src/main/java/com/astememe/openani/MainAnime.java
+++ app/src/main/java/com/astememe/openani/MainAnime.java
@@ -1,13 +1,18 @@
 package com.astememe.openani;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainAnime extends AppCompatActivity {
 
@@ -23,5 +28,24 @@ public class MainAnime extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+
+    public static class SostenDeVistas extends RecyclerView.ViewHolder {
+
+        TextView titulo_torrent, tamano_torrent, ultima_fecha_torrent, cantidad_seeders_torrent, cantidad_leechers_torrent, cantidad_likes_torrent, cantidad_dislikes_torrent, magner_boton_torrent;
+
+        public SostenDeVistas(@NonNull View itemView) {
+            super(itemView);
+            titulo_torrent = itemView.findViewById(R.id.titulo_torrent);
+            tamano_torrent = itemView.findViewById(R.id.tamano_torrent);
+            ultima_fecha_torrent = itemView.findViewById(R.id.fecha_actualizacion_torrent);
+            cantidad_seeders_torrent = itemView.findViewById(R.id.cantidad_seeders);
+            cantidad_leechers_torrent = itemView.findViewById(R.id.cantidad_leechers);
+            cantidad_likes_torrent = itemView.findViewById(R.id.cantidad_likes);
+            cantidad_dislikes_torrent = itemView.findViewById(R.id.cantidad_dislikes);
+            magner_boton_torrent = itemView.findViewById(R.id.magnet_boton);
+        }
     }
 }
