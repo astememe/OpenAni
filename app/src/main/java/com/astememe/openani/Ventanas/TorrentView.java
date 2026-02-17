050c742568c9202e5f95176b1867945154498f29
fabionick123 <fabioninahuanca62@gmail.com>
2026-02-17 11:10:14 +0100

    Funcionalidad añadida del boton hacia atras de TorrentView

--- app/src/main/java/com/astememe/openani/Ventanas/TorrentView.java
+++ app/src/main/java/com/astememe/openani/Ventanas/TorrentView.java
@@ -1,9 +1,12 @@
 package com.astememe.openani.Ventanas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,6 +15,7 @@ import com.astememe.openani.R;
 
 public class TorrentView extends AppCompatActivity {
 
+    ConstraintLayout flechaAtras;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,14 @@ public class TorrentView extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        flechaAtras = findViewById(R.id.flecha_volver_title);
+        flechaAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TorrentView.this, MainAnime.class);
+                startActivity(intent);
+            }
         });
     }
 }
