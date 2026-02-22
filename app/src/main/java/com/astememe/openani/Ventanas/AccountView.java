acb7b2e785dce259229380dde03ea9bf28fcf26e
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-22 18:37:13 +0100

    Se pueden modificar los usuarios ya

--- app/src/main/java/com/astememe/openani/Ventanas/AccountView.java
+++ app/src/main/java/com/astememe/openani/Ventanas/AccountView.java
@@ -1,16 +1,12 @@
 package com.astememe.openani.Ventanas;
 
-import static android.view.LayoutInflater.from;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
+
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -19,6 +15,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,18 +24,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.astememe.openani.Django_Manager.Interfaces.DjangoClient;
+import com.astememe.openani.Django_Manager.Models.UserDataModel;
+import com.astememe.openani.Django_Manager.Models.UserUpdateModel;
 import com.astememe.openani.R;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import io.woong.shapedimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Response;
 
-import java.io.File;
-
-import io.woong.shapedimageview.CircleImageView;
 
 public class AccountView extends AppCompatActivity {
 
@@ -47,16 +45,13 @@ public class AccountView extends AppCompatActivity {
     //https://www.reddit.com/r/learnprogramming/comments/q5xelq/should_i_convert_an_image_uri_to_a_blobbytearray/
 
     ConstraintLayout botonAtras;
-    EditText descripcion;
+    EditText descripcion, nombreUsuario, correo, contrasena;
     CircleImageView fotoPerfil;
-
-    View menuCambiarFoto;
-
-    ImageView cerrarMenu;
     LinearLayout primeraFoto, segundaFoto, terceraFoto, cuartaFoto, quintaFoto, sextaFoto, septimaFoto, octavaFoto, novenaFoto, decimaFoto, decimoPrimeraFoto, decimoSegundaFoto, decimoTerceraFoto, decimoCuartaFoto, decimoQuintaFoto;
     View botonCambiarFoto;
     LayoutInflater infladorDeCambiarFoto;
     LinearLayout layoutInflateAcountViewReference;
+    ConstraintLayout botonGuardar;
 
     protected void onCreate(Bundle savedInstanceState) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -71,6 +66,14 @@ public class AccountView extends AppCompatActivity {
         fotoPerfil = findViewById(R.id.fotoperfil);
         descripcion = findViewById(R.id.descripcion);
 
+        nombreUsuario = findViewById(R.id.nombreusuario_account);
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+        botonGuardar = findViewById(R.id.guardarcambios_account);
+
+        nombreUsuario.setText(preferences.getString("nombre", ""));
+        correo.setText(preferences.getString("email", ""));
+
         layoutInflateAcountViewReference = findViewById(R.id.contenedorMenuFotoPerfil);
         botonAtras = findViewById(R.id.flechaAtrasAcountView);
         descripcion.setText(preferences.getString("descripcion", "Escriba su descripción aquí"));
@@ -84,12 +87,30 @@ public class AccountView extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        if (preferences.getString("imagen", "").isEmpty()) {
-            fotoPerfil.setImageResource(R.drawable.spiderman);
-        } else {
-            Uri uri = Uri.parse("android.resource://" + this.getPackageName() + "/drawable/foto_de_perfil_" + preferences.getString("imagen", ""));
-            fotoPerfil.setImageURI(uri);
-        }
+
+        Uri uri = Uri.parse("android.resource://" + this.getPackageName() + "/drawable/foto_de_perfil_" + preferences.getString("imagen", ""));
+        fotoPerfil.setImageURI(uri);
+
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = nombreUsuario.getText().toString();
+                String email = correo.getText().toString();
+                String password = contrasena.getText().toString();
+                String descripcion = AccountView.this.descripcion.getText().toString();
+                String imagen = preferences.getString("imagen", "");
+
+                if (password.isEmpty()){
+                    password = null;
+                }
+
+                UserUpdateModel userUpdateModel = new UserUpdateModel(username, email, password, imagen, descripcion);
+                String token = "Bearer " + preferences.getString("token", "");
+
+                guardarCambios();
+
+            }
+        });
 
         botonCambiarFoto.setOnClickListener(v -> {
             layoutInflateAcountViewReference.removeAllViews();
@@ -122,10 +143,11 @@ public class AccountView extends AppCompatActivity {
                         String valor = nombreFoto.getText().toString();
                         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                         sharedPreferences.edit().putString("imagen", valor).apply();
+                        fotoPerfil.setImageURI(Uri.parse("android.resource://" + getPackageName() + "/drawable/foto_de_perfil_" + valor));
                         desplazarMenu(slide_out);
                     }
                 });
-            };
+            }
         });
     }
 
@@ -138,4 +160,42 @@ public class AccountView extends AppCompatActivity {
             }
         }, slide_out.getDuration());
     }
+
+    public void guardarCambios(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String token = "Bearer " + prefs.getString("token", "");
+
+
+        String nuevoNombre = nombreUsuario.getText().toString().trim();
+        String nuevoCorreo = correo.getText().toString().trim();
+        String nuevaContrasena = contrasena.getText().toString().trim();
+        String nuevaDescripcion = descripcion.getText().toString().trim();
+        String nuevaImagen = prefs.getString("imagen", "");
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("nombre", nuevoNombre);
+        editor.putString("email", nuevoCorreo);
+        editor.putString("descripcion", nuevaDescripcion);
+        editor.putString("imagen", nuevaImagen);
+
+        UserUpdateModel userUpdateModel = new UserUpdateModel(nuevoNombre, nuevoCorreo, nuevaContrasena, nuevaImagen, nuevaDescripcion);
+
+        DjangoClient.getUserAPI_Interface().updateProfile(token, userUpdateModel).enqueue(new retrofit2.Callback<UserDataModel>() {
+
+            @Override
+            public void onResponse(Call<UserDataModel> call, Response<UserDataModel> response) {
+                editor.apply();
+
+                Toast.makeText(AccountView.this, "¡Cambios guardados con éxito!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<UserDataModel> call, Throwable t) {
+                Toast.makeText(AccountView.this, "Error: El usuario ya existen", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
 }
