f4bf0726b0dc2c06ae42f27ccad9cef9eab58fe0
StoianAlejandro <s.alejandrostoian@gmail.com>
2026-02-26 10:19:05 +0100

    Añadido control si es invitado a los comentarios

--- app/src/main/java/com/astememe/openani/Ventanas/TorrentView.java
+++ app/src/main/java/com/astememe/openani/Ventanas/TorrentView.java
@@ -119,6 +119,10 @@ public class TorrentView extends AppCompatActivity {
         anadir_comentario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (preferences.getBoolean("invitado", true)) {
+                    Toast.makeText(getApplicationContext(), "You must Log In to comment", Toast.LENGTH_LONG).show();
+                    return;
+                }
                 mostrarMensajeComent();
             }
         });
