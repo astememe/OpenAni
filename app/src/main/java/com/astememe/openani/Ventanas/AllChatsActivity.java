b0de5737e638fbe27999c9327f248af5afd3e673
rayden <raydenldon@gmail.com>
2026-05-10 18:07:48 +0200

    card de usuario para chats creado

--- app/src/main/java/com/astememe/openani/Ventanas/AllChatsActivity.java
+++ app/src/main/java/com/astememe/openani/Ventanas/AllChatsActivity.java
@@ -1,4 +1,4 @@
-package com.astememe.openani;
+package com.astememe.openani.Ventanas;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_other_profile extends AppCompatActivity {
+import com.astememe.openani.R;
+
+public class AllChatsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_other_profile);
+        setContentView(R.layout.activity_all_chats);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
